4b1a030d0d6c4ee6b07eeb127eace597be19a58b
Programador Lhama <programadorlhama@gmail.com>
2020-12-13 12:07:27 -0300

    test: Testing select_user from UserRepository

--- src/models/repositorys/user_repository_test.py
+++ src/models/repositorys/user_repository_test.py
@@ -1,4 +1,5 @@
 from faker import Faker
+from src.models.entities import Users
 from src.models.configs import DBConnectionHandler
 from .user_repository import UserRepository
 
@@ -25,3 +26,28 @@ def test_insert_user():
     assert new_user.password == query_user.password
 
     engine.execute("DELETE FROM users WHERE id='{}';".format(new_user.id))
+
+
+def test_select_user():
+    """ Should select a user in Users table compare it """
+
+    user_id = faker.random_number(digits=5)
+    name = faker.name()
+    password = faker.word()
+    data = Users(id=user_id, name=name, password=password)
+    engine = db_connection_handler.get_engine()
+
+    engine.execute(
+        "INSERT INTO users (id, name, password) VALUES ('{}','{}', '{}');".format(
+            user_id, name, password
+        )
+    )
+    query_users1 = user_repository.select_user(user_id=user_id)
+    query_users2 = user_repository.select_user(name=name)
+    query_users3 = user_repository.select_user(user_id=user_id, name=name)
+
+    assert data in query_users1
+    assert data in query_users2
+    assert data in query_users3
+
+    engine.execute("DELETE FROM users WHERE id='{}';".format(user_id))

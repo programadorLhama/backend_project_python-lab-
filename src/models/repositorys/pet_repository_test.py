d00905af3b229ba222ed7e3bb6b50ce5a1684c89
Programador Lhama <programadorlhama@gmail.com>
2020-12-14 00:12:58 -0300

    test: Testing insert_pet method from PetRepository

---
+++ src/models/repositorys/pet_repository_test.py
@@ -0,0 +1,31 @@
+from faker import Faker
+from src.models.configs import DBConnectionHandler
+from .pet_repository import PetRepository
+
+faker = Faker()
+db_connection_handler = DBConnectionHandler()
+pet_repository = PetRepository()
+
+
+def test_insert_pet():
+    """ Should insert user in Users table and get it """
+
+    name = faker.name()
+    specie = "fish"
+    age = faker.random_number(digits=1)
+    user_id = faker.random_number()
+    engine = db_connection_handler.get_engine()
+
+    # SQL commands
+    new_pet = pet_repository.insert_pet(name, specie, age, user_id)
+    query_user = engine.execute(
+        "SELECT * FROM pets WHERE id='{}';".format(new_pet.id)
+    ).fetchone()
+
+    assert new_pet.id == query_user.id
+    assert new_pet.name == query_user.name
+    assert new_pet.specie == query_user.specie
+    assert new_pet.age == query_user.age
+    assert new_pet.user_id == query_user.user_id
+
+    engine.execute("DELETE FROM pets WHERE id='{}';".format(new_pet.id))

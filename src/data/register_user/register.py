95527fce6c1f58d31dd33590b382856e33383a00
Programador Lhama <programadorlhama@gmail.com>
2020-12-19 20:41:24 -0300

    feat: Imlementing RegisterUser usecase

---
+++ src/data/register_user/register.py
@@ -0,0 +1,24 @@
+from typing import Dict, Type
+from src.models import UserRepository
+
+
+class RegisterUser:
+    """ Class to define usecase: Register User """
+
+    def __init__(self, user_repository: Type[UserRepository]):
+        self.user_repository = user_repository
+
+    def registry(self, name: str, password: str) -> Dict[str, str]:
+        """Registry user
+        :param  - name: person name
+                - password: password of the person
+        :return - Dictionary with sucess of the process
+        """
+
+        response = None
+        validate_entry = isinstance(name, str) and isinstance(password, str)
+
+        if validate_entry:
+            response = self.user_repository.insert_user(name, password)
+
+        return {"Success": validate_entry, "Data": response}

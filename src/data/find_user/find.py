abd08d7c25f6f87169a9f01fc57bad87cf8f324c
Programador Lhama <programadorlhama@gmail.com>
2020-12-19 22:25:40 -0300

    feat: implementing by_id_and_user in FindUser usecase and testing it

--- src/data/find_user/find.py
+++ src/data/find_user/find.py
@@ -35,3 +35,17 @@ class FindUser:
             response = self.user_repository.select_user(name=name)
 
         return {"Success": validate_entry, "Data": response}
+
+    def by_id_and_user(self, user_id: int, name: str) -> Dict[str, str]:
+        """Select User By name
+        :param - name: name of the user
+        :return - Dictionary with informations of the process
+        """
+
+        response = None
+        validate_entry = isinstance(user_id, int) and isinstance(name, str)
+
+        if validate_entry:
+            response = self.user_repository.select_user(user_id=user_id, name=name)
+
+        return {"Success": validate_entry, "Data": response}
--- src/data/find_user/find_test.py
+++ src/data/find_user/find_test.py
@@ -57,13 +57,57 @@ def test_by_name():
 
 
 def test_fail_by_name():
-    """ Testing by_name method in FindUser """
+    """ Testing by_name fail method in FindUser """
 
     user_repo = UserRepositorySpy()
     find_user = FindUser(user_repo)
 
     attribute = {"name": faker.random_number(digits=2)}
     response = find_user.by_name(name=attribute["name"])
+
+    # Testing Input
+    assert user_repo.select_user_params == {}
+
+    # Testing Outputs
+    assert response["Success"] is False
+    assert response["Data"] is None
+
+
+def test_by_id_and_user():
+    """ Testing by_id_and_user method in FindUser """
+
+    user_repo = UserRepositorySpy()
+    find_user = FindUser(user_repo)
+
+    attribute = {"user_id": faker.random_number(digits=2), "name": faker.word()}
+
+    response = find_user.by_id_and_user(
+        user_id=attribute["user_id"], name=attribute["name"]
+    )
+
+    # Testing Input
+    assert user_repo.select_user_params["user_id"] == attribute["user_id"]
+    assert user_repo.select_user_params["name"] == attribute["name"]
+
+    # Testing Outputs
+    assert response["Success"] is True
+    assert response["Data"]
+
+
+def test_fail_by_id_and_user():
+    """ Testing by_id_and_user fail method in FindUser """
+
+    user_repo = UserRepositorySpy()
+    find_user = FindUser(user_repo)
+
+    attribute = {
+        "user_id": faker.random_number(digits=2),
+        "name": faker.random_number(digits=2),
+    }
+
+    response = find_user.by_id_and_user(
+        user_id=attribute["user_id"], name=attribute["name"]
+    )
 
     # Testing Input
     assert user_repo.select_user_params == {}

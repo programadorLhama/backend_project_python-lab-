1742ad2e59695072333c45f8ff073aabeb0964f2
Programador Lhama <programadorlhama@gmail.com>
2020-12-19 23:18:09 -0300

    feat: implementing pet_id_and_user_id in FindPet usecase

--- src/data/find_pet/find.py
+++ src/data/find_pet/find.py
@@ -8,6 +8,20 @@ class FindPet:
     def __init__(self, pet_repository: Type[PetRepository]):
         self.pet_repository = pet_repository
 
+    def by_pet_id(self, pet_id: int) -> Dict[str, str]:
+        """Select Pet By pet_id
+        :param - pet_id: id of the pet
+        :return - Dictionary with informations of the process
+        """
+
+        response = None
+        validate_entry = isinstance(pet_id, int)
+
+        if validate_entry:
+            response = self.pet_repository.select_pet(pet_id=pet_id)
+
+        return {"Success": validate_entry, "Data": response}
+
     def by_user_id(self, user_id: int) -> Dict[str, str]:
         """Select Pet By user_id
         :param - user_id: id of the user owne of the pet
@@ -19,5 +33,19 @@ class FindPet:
 
         if validate_entry:
             response = self.pet_repository.select_pet(user_id=user_id)
+
+        return {"Success": validate_entry, "Data": response}
+
+    def by_pet_id_and_user_id(self, pet_id: int, user_id: int) -> Dict[str, str]:
+        """Select Pet By user_id
+        :param - user_id: id of the user owne of the pet
+        :return - Dictionary with informations of the process
+        """
+
+        response = None
+        validate_entry = isinstance(user_id, int) and isinstance(pet_id, int)
+
+        if validate_entry:
+            response = self.pet_repository.select_pet(pet_id=pet_id, user_id=user_id)
 
         return {"Success": validate_entry, "Data": response}
